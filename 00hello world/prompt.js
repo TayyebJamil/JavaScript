00d0d0cde17e3f88042512f87b6041816a6d5a07
TayyebJamil <its.tayyeb.ch159@gmail.com>
2024-01-31 19:28:03 +0500

    practice projects

---
+++ 00hello world/prompt.js
@@ -0,0 +1,14 @@
+//var name = prompt("what is your name ?","jamil")
+//console.log(name)
+
+var age =prompt('what is you age ', 'Enter integer value')
+console.log(age)
+var a= parseInt(age)
+var newage=prompt('what is your newage','enter integer number')
+console.log(newage) 
+var b= parseInt(newage)
+
+//var sum=parseInt(age)+parseInt(newage)
+//var sum=parseFloat(age)+parseFloat(newage)
+var sum= a+b
+document.write(sum)
